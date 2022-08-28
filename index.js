17607c6c6db67dbb1256f8af4f3f2166ec3d44c2
hlnicks <hlnicks@icloud.com>
2022-08-27 22:06:02 -0400

    created questions for engineer, intern and manager

--- index.js
+++ index.js
@@ -1,7 +1,84 @@
 const inquirer = import("inquirer");
 
-// questions
+// questions prompt
+promptUser = function() {
+    inquirer.prompt(
+        {
+            type: "list",
+            name: "employee",
+            message: "Which type of employee would you like to create?",
+            choices: ["Engineer", "Intern", "Manager", "None"]
+        }
+    )
+}
 
-const engineerQuestions = []
-const internQuestions = []
-const managerQuestions = []
+// questions
+// will need to validate input. return true; and return false;
+const engineerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email address?"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is the engineer's Github Username?"
+    }
+];
+
+const internQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the intern's ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email address?"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What is the intern's school?"
+    }
+];
+
+const managerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the manager's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the manager's ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email address?"
+    },
+    {
+        type: "input",
+        name: "office",
+        message: "What is the manager's office number?"
+    }
+];

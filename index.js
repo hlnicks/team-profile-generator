376fd372500c0003a871f7133b8ffd1e91e0cff2
hlnicks <hlnicks@icloud.com>
2022-08-30 22:20:41 -0400

    started function that allows user to select what kind of employee they would like to create

--- index.js
+++ index.js
@@ -5,85 +5,14 @@ const inquirer = require("inquirer");
 // const createCard = require("./src/page-template");
 // const {writeFile, copyFile} = require("./src/page-template");
 
+function myEmployees() {
+    this.employee = [];
+    this.manager;
+    this.engineer;
+    this.intern;
+}
 
-const engineerQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is the engineer's name?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is the engineer's ID?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is the engineer's email address?"
-    },
-    {
-        type: "input",
-        name: "username",
-        message: "What is the engineer's Github Username?"
-    }
-];
-console.log(engineerQuestions);
-
-
-const internQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is the intern's name?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is the intern's ID?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is the intern's email address?"
-    },
-    {
-        type: "input",
-        name: "school",
-        message: "What is the intern's school?"
-    }
-];
-console.log(internQuestions);
-
-
-const managerQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is the manager's name?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is the manager's ID?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is the manager's email address?"
-    },
-    {
-        type: "input",
-        name: "office",
-        message: "What is the manager's office number?"
-    }
-];
-console.log(managerQuestions);
-
-
-
-// questions prompt
-promptUser = function() {
+myEmployees.prototype.promptUser = function() {
     inquirer.prompt(
         {
             type: "list",
@@ -93,3 +22,80 @@ promptUser = function() {
         }
     )
 };
+
+new myEmployees().promptUser();
+
+
+// const engineerQuestions = [
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "What is the engineer's name?"
+//     },
+//     {
+//         type: "input",
+//         name: "id",
+//         message: "What is the engineer's ID?"
+//     },
+//     {
+//         type: "input",
+//         name: "email",
+//         message: "What is the engineer's email address?"
+//     },
+//     {
+//         type: "input",
+//         name: "username",
+//         message: "What is the engineer's Github Username?"
+//     }
+// ];
+// console.log(engineerQuestions);
+
+
+// const internQuestions = [
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "What is the intern's name?"
+//     },
+//     {
+//         type: "input",
+//         name: "id",
+//         message: "What is the intern's ID?"
+//     },
+//     {
+//         type: "input",
+//         name: "email",
+//         message: "What is the intern's email address?"
+//     },
+//     {
+//         type: "input",
+//         name: "school",
+//         message: "What is the intern's school?"
+//     }
+// ];
+// console.log(internQuestions);
+
+
+// const managerQuestions = [
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "What is the manager's name?"
+//     },
+//     {
+//         type: "input",
+//         name: "id",
+//         message: "What is the manager's ID?"
+//     },
+//     {
+//         type: "input",
+//         name: "email",
+//         message: "What is the manager's email address?"
+//     },
+//     {
+//         type: "input",
+//         name: "office",
+//         message: "What is the manager's office number?"
+//     }
+// ];
+// console.log(managerQuestions);
